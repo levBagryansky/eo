bd317a4faffb9798362261548ca824b55e027904
levBagryansky <28lev11@gmail.com>
2024-02-20 18:13:57 +0300

    #2879: IRBuilder

--- eo-maven-plugin/src/main/java/org/eolang/maven/IRmanipulating/IRBuilder.java
+++ eo-maven-plugin/src/main/java/org/eolang/maven/IRmanipulating/IRBuilder.java
@@ -1,34 +1,11 @@
 package org.eolang.maven.IRmanipulating;
 
-import java.util.ArrayList;
-import java.util.List;
+public interface IRBuilder {
+    IRSimpleBuilder with(final String name, final Expression expression);
 
-public class IRBuilder {
+    IRSimpleBuilder with(final BoundAttribute bound);
 
-    final List<BoundAttribute> attrs;
+    Link getLinkTo(final String name);
 
-    public IRBuilder() {
-        this.attrs = new ArrayList<>();
-    }
-
-    public IRBuilder with(final String name, final Expression expression) {
-        this.with(new BoundAttribute(name, expression));
-        return this;
-    }
-
-    public IRBuilder with(final BoundAttribute bound) {
-        this.attrs.add(bound);
-        return this;
-    }
-
-    public Link getLinkTo(final String name) {
-        final Attribute source = attrs.stream().filter(attr -> name.equals(attr.name())).findFirst().orElseThrow(
-            () -> new IllegalArgumentException(String.format("No attribute %s found", name))
-        );
-        return new Link(source);
-    }
-
-    public IR build() {
-        return new IR(attrs);
-    }
+    IR build();
 }
---
+++ eo-maven-plugin/src/main/java/org/eolang/maven/IRmanipulating/IRSimpleBuilder.java
@@ -0,0 +1,34 @@
+package org.eolang.maven.IRmanipulating;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IRSimpleBuilder implements IRBuilder {
+
+    final List<BoundAttribute> attrs;
+
+    public IRSimpleBuilder() {
+        this.attrs = new ArrayList<>();
+    }
+
+    public IRSimpleBuilder with(final String name, final Expression expression) {
+        this.with(new BoundAttribute(name, expression));
+        return this;
+    }
+
+    public IRSimpleBuilder with(final BoundAttribute bound) {
+        this.attrs.add(bound);
+        return this;
+    }
+
+    public Link getLinkTo(final String name) {
+        final Attribute source = attrs.stream().filter(attr -> name.equals(attr.name())).findFirst().orElseThrow(
+            () -> new IllegalArgumentException(String.format("No attribute %s found", name))
+        );
+        return new Link(source);
+    }
+
+    public IR build() {
+        return new IR(attrs);
+    }
+}
